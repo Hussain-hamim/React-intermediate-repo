b111325a9e514aa7b1e5f724023cbdd1510746b7
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-06-02 20:33:56 +0430

    only render needed render

--- src/state-management/counter/Counter.tsx
+++ src/state-management/counter/Counter.tsx
@@ -5,11 +5,12 @@ const Counter = () => {
    * in a component
    */
   // const [value, dispatch] = useReducer(counterReducer, 0);
-  const { counter, increment, reset } = useCounterStore(); //  Zustand
+  const { increment, reset } = useCounterStore(); //  Zustand
+  const counter = useCounterStore((s) => s.counter); //  Zustand will render it only when the counter value changes
 
   return (
     <div>
-      Counter ({counter})
+      <span style={{ color: "whitesmoke" }}>Counter ({counter})</span>
       <button onClick={() => increment()} className="btn btn-primary mx-1">
         Increment
       </button>
