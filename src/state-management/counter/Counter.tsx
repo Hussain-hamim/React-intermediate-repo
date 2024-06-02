7ee4e016e27eba85e0ea73d16bb3a82e106129c0
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-06-02 08:44:12 +0430

    split the application by functional area

--- src/state-management/counter/Counter.tsx
+++ src/state-management/counter/Counter.tsx
@@ -1,5 +1,5 @@
 import { useReducer, useState } from "react";
-import counterReducer from "./reducers/counterReducer";
+import counterReducer from "./counterReducer";
 
 const Counter = () => {
   /** Reducer: a function that allow us to centralize state updates
